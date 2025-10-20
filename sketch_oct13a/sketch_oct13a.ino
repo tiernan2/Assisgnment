91c28cfbcc26f48118b5bc5ad1a10711ffdefb6b
fiachra04 <162336386+fiachra04@users.noreply.github.com>
2025-10-20 09:27:40 +0100

    Update sketch_oct13a.ino

--- sketch_oct13a/sketch_oct13a.ino
+++ sketch_oct13a/sketch_oct13a.ino
@@ -1,12 +1,12 @@
-const int buttonPin = A2; //Assigns A2 to be the button
-const int ledPin = A0; 
-const int buzzerPin = 4;
+const int buttonPin = A2; //Assigns the Button to port A2
+const int ledPin = A0; //Assigns the LED to port A0
+const int buzzerPin = 4; //Assigns the buzzer to port D4
 
 bool lastButtonState = HIGH; //boolean values must be either true or false.(The output is either on or its not, no inbetween)
 bool waitingToStart = false;
 bool outputOn = false;
 
-unsigned long pressTime = 0;  //ensures a posotive number is stored
+unsigned long pressTime = 0;  //ensures a positive number is stored
 unsigned long outputStartTime = 0;
 
 void setup() { //This is the defualt start position. there is no light or buzzer on
@@ -18,7 +18,7 @@ void setup() { //This is the defualt start position. there is no light or buzzer
   noTone(buzzerPin); // buzzer off
 
   Serial.begin(9600); //
-  Serial.println("red light"); 
+  Serial.println("red light")
 }
 
 void loop() {
@@ -36,7 +36,7 @@ void loop() {
 
   // After 5 seconds from button press, turn on LED and buzzer
   if (waitingToStart && (millis() - pressTime >= 5000)) { // waits 5 seconds
-    Serial.println("Button was pressed!"); //text appears in the serial monitor to indicate the start of the cycle
+    Serial.println("Button was Pressed!"); //text appears in the serial monitor to indicate the start of the cycle
     digitalWrite(ledPin, HIGH); //led comes on
     tone(buzzerPin, 1000); // buzzer comes on
     outputStartTime = millis();
